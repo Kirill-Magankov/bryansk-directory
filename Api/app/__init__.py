fdc6ca9ad989f9e2d90d631d1103c41e814d29f3
Kirill-Magankov <kirill.magankov@gmail.com>
2023-10-20 19:41:00 +0300

    Added Long polling logics

--- Api/app/__init__.py
+++ Api/app/__init__.py
@@ -1,6 +1,11 @@
+import threading
+import time
+from urllib import request
+from urllib.error import HTTPError
+
 from flask import Flask, redirect
 from flask_cors import CORS
-from healthcheck import HealthCheck, EnvironmentDump
+from healthcheck import HealthCheck
 from sqlalchemy import text
 
 from app.utils.blocklist import BLOCKLIST
@@ -51,3 +56,77 @@ app.add_url_rule(f'{app.config.get("API_URL")}/health', 'health', view_func=lamb
 
 @app.route('/')
 def index(): return redirect(app.config.get('API_URL'))
+
+
+class DataModelList:
+    class DataModel:
+        def __init__(self, id_, user_id, message, status='Open', comment=None):
+            self.id = id_
+            self.user_id = user_id
+            self.message = message
+            self.status = status
+            self.comment = comment
+
+    datas = [
+        DataModel(1, 8, 'The feedback text # 1'),
+        DataModel(2, 12, 'The feedback text # 2'),
+        DataModel(3, 12, 'The feedback text # 3'),
+        DataModel(4, 13, 'The feedback text # 4'),
+    ]
+
+    def get(self, id_):
+        if data := [i for i in self.datas if i.id == id_]:
+            if len(data) >= 0: return data[0]
+        return None
+
+
+TIMEOUT = 30
+
+data_list = DataModelList()
+
+
+@app.route('/feedback-notify/<int:id_>')
+def poll(id_):
+    start = time.time()
+    while time.time() - start < TIMEOUT:
+        entry = data_list.get(id_)
+        if not entry: return {'message': 'No available data'}, 400
+
+        if entry.status == 'closed':
+            return {'message': 'Feedback No. %i closed.' % entry.id,
+                    'data': {
+                        'user_id': entry.user_id,
+                        'comment': entry.comment
+                    }}
+        time.sleep(2)
+
+    return {}, 204
+
+
+@app.route('/closeFeedback/<int:id_>')
+def close_feedback(id_):
+    entry = data_list.get(id_)
+    if not entry: return {'message': 'No data to display'}
+    if entry.status == 'closed': return {'message': 'Feedback already closed.'}
+    entry.status = 'closed'
+    print(entry.__dict__)
+    return {'message': 'Feedback No. %i closed.' % entry.id}
+
+
+@app.route('/openFeedback/<int:id_>')
+def open_feedback(id_):
+    entry = data_list.get(id_)
+    if not entry: return {'message': 'No data to display'}
+    if entry.status == 'open': return {'message': 'Feedback already open.'}
+    entry.status = 'open'
+    print(entry.__dict__)
+    return {'message': 'Feedback No. %i open.' % entry.id}
+
+
+@app.route('/client')
+def client_test():
+    try:
+        response = request.urlopen('http://localhost:8000/feedback-notify/1', timeout=TIMEOUT)
+        return response
+    except TimeoutError:
+        client_test()
