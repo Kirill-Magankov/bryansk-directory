588b01f9aee2868a1bf9f61193a084151580f0f1
Kirill-Magankov <kirill.magankov@gmail.com>
2023-10-23 15:50:11 +0300

    Added local database (sqlite). Updated requirements.txt

--- Api/config.py
+++ Api/config.py
@@ -10,7 +10,8 @@ class Config:
     MYSQL_USER = os.getenv('MYSQL_USER')
     MYSQL_PASS = os.getenv('MYSQL_PASS')
     DATABASE_URI = os.getenv('DATABASE_URI')
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASS}@{DATABASE_URI}'
+    # SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASS}@{DATABASE_URI}'
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///database.db'
 
     API_VERSION = '0.1'
     API_TITLE = 'Справочник города Брянска'
